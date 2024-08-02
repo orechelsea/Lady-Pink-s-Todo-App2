6dce86052df51dadfbfe3dcd932e285131b8ee82
Chelsea Osayomi <orechelsea04@gmail.com>
2024-08-02 15:41:37 +0000

    "created the todo app url, wrote my index.html view and added the todo app url in the project url"

--- Todo/views.py
+++ Todo/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Todo
 # Create your views here.
-def Todo(request):
-    return HttpResponse("Hello, Blog!")
+
+def index(request):
+    context = {
+        'greeting': "Hello! and welcome to Lady Pink's Todo List App to keep you from pulling your hair out and get you organised!!! What are you waiting for? Sign Up Below to join thousands of Pinkies in the organisation frenzy!!",
+        'about': "Empower yourself through Lady Pink's Todo App because it allows you to take charge of your own life. Stay organised, stay focused, and achieve your goals with ease!",
+        'footer_message': "Show us some love on our social media platforms, get some new inspiration on how to schedule your time effectively and get all the cool features our app has to offer.",
+    }
+
+    return render(request, 'todo/index.html', context)
--- ladypink/urls.py
+++ ladypink/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from Todo.views import Todo
+from django.urls import path, include
+
 
 urlpatterns = [
-    path('Todo/', Todo, name='Todo'),
+    path("", include("Todo.urls"), name="Todo-urls"),
     path('admin/', admin.site.urls),
 ]
