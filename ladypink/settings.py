78476cd80e94b0e485a006c6eb0694056ba2f067
Chelsea Osayomi <orechelsea04@gmail.com>
2024-08-02 15:13:35 +0000

    "added my secret key to settings and personalised the secret key"

--- ladypink/settings.py
+++ ladypink/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=c7#80vmcu-ielnh_7ic46p*n_1e)=lekt4m62%$)ck$n4yx8b'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
