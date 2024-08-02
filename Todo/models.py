e97a456cec38389dc6aec80d13d09d405d1a0ba7
Chelsea Osayomi <orechelsea04@gmail.com>
2024-08-02 15:21:23 +0000

    "added my model for the app and migrated"

--- Todo/models.py
+++ Todo/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+Priority_Choices = [
+    ('low', 'low'),
+    ('medium', 'medium'),
+    ('high', 'high'),
+]
+
+class Todo(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=200, null=True)
+    description = models.TextField(null=True, blank=True)
+    priority = models.CharField(max_length=10, choices=Priority_Choices, default='low')
+    complete = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['complete']
