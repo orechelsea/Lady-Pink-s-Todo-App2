aaf9072691c38a00b657881586f9ba4bef5cebe9
Chelsea Osayomi <orechelsea04@gmail.com>
2024-08-03 14:30:36 +0000

    "used the template provided in the walkthrough for the login and logout page"

--- ladypink/urls.py
+++ ladypink/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Todo.urls')),
+    path("accounts/", include("allauth.urls")),
 ]
