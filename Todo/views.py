d74bb808dcaae6f14afa1b02416fab5ee75fe6eb
Chelsea Osayomi <orechelsea04@gmail.com>
2024-08-03 12:12:03 +0000

    "fixed the social media links that were too close together and the specificity of the styling and added some more text to the view.py"

--- Todo/views.py
+++ Todo/views.py
@@ -5,7 +5,7 @@ from .models import Todo
 def index(request):
     context = {
         'greeting': "Hello! and welcome to Lady Pink's Todo List App to keep you from pulling your hair out and get you organised!!! What are you waiting for? Sign Up Below to join thousands of Pinkies in the organisation frenzy!!",
-        'about': "Empower yourself through Lady Pink's Todo App because it allows you to take charge of your own life. Stay organised, stay focused, and achieve your goals with ease!",
+        'about': "Empower yourself through Lady Pink's Todo App because it allows you to take charge of your own life. Stay organised, stay focused, and achieve your goals with ease! Our app is designed to help you manage your tasks efficiently, giving you more time for what truly matters. Whether you're a student, a professional, or a busy parent, Lady Pink's Todo App is here to simplify your life. Track your progress, set reminders, and never miss a deadline again. Join thousands of Pinkies who have transformed their lives by staying on top of their schedules. With Lady Pink's Todo App, you'll find balance, boost productivity, and achieve more every day. Let's make organization stylish, fun, and incredibly effective!",
         'footer_message': "Show us some love on our social media platforms, get some new inspiration on how to schedule your time effectively and get all the cool features our app has to offer.",
     }
 
