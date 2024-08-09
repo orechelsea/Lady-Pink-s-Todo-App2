8a50d58a0d65297158a901d57cbf8c52b85e2073
Chelsea Osayomi <orechelsea04@gmail.com>
2024-08-09 11:56:10 +0000

    "was finally able to add the edit button to existing todo items, still needs to be adjusted so that it stops saving the updated todo item as new ones but finding that difficult now"

--- Todo/urls.py
+++ Todo/urls.py
@@ -2,11 +2,10 @@ from django.urls import path
 from . views import update_todo_view, delete_todo_view
 from . import views
 
-
 urlpatterns = [
-    path('', views.index, name='index'), 
-    path('update/<int:todo_id>/', views.update_todo_view, name='update_todo'),
-    path('update_complete/<int:todo_id>/', views.update_todo_complete, name='update_todo_complete'),
-    path('update/', update_todo_view, name='update_todo'),
-    path('delete/<int:todo_id>/', delete_todo_view, name='delete_todo'),
+    path('', views.index, name='index'),
+    path('update_todo/', views.update_todo_view, name='update_todo'),
+    path('update_todo/<int:todo_id>/', views.update_todo_view, name='update_todo'),
+    path('update_todo_complete/<int:todo_id>/', views.update_todo_complete, name='update_todo_complete'),
+    path('delete_todo/<int:todo_id>/', views.delete_todo_view, name='delete_todo'),
 ]
