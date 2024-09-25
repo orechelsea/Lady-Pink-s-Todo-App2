3d3755fb96c401e09e62fdf65d7190aad2e6ad01
Chelsea Osayomi <orechelsea04@gmail.com>
2024-09-25 12:42:25 +0000

    deleted something so i could set debug to false

--- ladypink/settings.py
+++ ladypink/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # Needs to = False before submission
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
